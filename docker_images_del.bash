21d6d4c4d07417c921c28627539857b217734d50
Quang Chu <chuvanquang0104@gmail.com>
2019-12-29 20:36:35 +0700

    Creating swagger for product-composite

--- docker_images_del.bash
+++ docker_images_del.bash
@@ -3,3 +3,4 @@ docker image rm spring_product-composite:latest;
 docker image rm spring_product:latest;
 docker image rm spring_recommendation:latest;
 docker image rm spring_review:latest;
+./gradlew build && docker-compose build && docker-compose up -d
