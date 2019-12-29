dc093ab2e0b0e3c822cfe22f28df2a6285f1416c
Quang Chu <chuvanquang0104@gmail.com>
2019-12-29 19:39:50 +0700

    Docker-compose deployment & test modify

---
+++ docker_images_del.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+docker image rm spring_product-composite:latest;
+docker image rm spring_product:latest;
+docker image rm spring_recommendation:latest;
+docker image rm spring_review:latest;
